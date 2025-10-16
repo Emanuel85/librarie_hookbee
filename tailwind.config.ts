5056e63f29b71cbf1b049af5d6b9649e0e8a72c5
emanuelquintanilla85@gmail.com <emanuelquintanilla85@gmail.comm>
2025-10-16 07:26:27 -0300

    fix: ajuste de safelist

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx,mdx}"
-    ],
-    theme: {
-        extend: {
-            colors: {
-                primary: '#3b82f6',
-                secondary: '#64748b'
-            }
-        }
-    },
-    plugins: [],
-}
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./safelist.txt"
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        13: "repeat(13, minmax(0, 1fr))"
+      }
+    }
+  },
+  plugins: []
+};
